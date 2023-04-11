de1e00e7cf94a946e15761a3d3659af5e1ffe8c4
wesley oliveira <wlwwesley9@gmail.com>
2023-04-11 20:58:24 -0300

    activity shape

--- app/src/main/java/com/example/kanyerest/MainActivity.kt
+++ app/src/main/java/com/example/kanyerest/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.kanyerest
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.example.kanyerest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        val tvQuotes = binding.tvQuotes
+        val btnQuotesSearch = binding.btnQuotesSearch
     }
 }
